2889c969f2eebae1e3b65af3dd80c97d69614bda
protongamer <46281599+protongamer@users.noreply.github.com>
2022-07-28 16:41:32 +0200

    Update TinyScope.ino

--- TinyScope/TinyScope.ino
+++ TinyScope/TinyScope.ino
@@ -19,17 +19,24 @@
 
 //////////////////////////////////////////////
 
-#define BMP_BUF               128
+#define TEXT_FONT_USED             u8g2_font_squeezed_b7_tr
+#define TEXT_CURSOR_X              5
+#define TEXT_CURSOR_Y              7
 
-#define ADC_MAX               1023.0
-#define SCOPE_HEIGHT          64.0
+
+#define BMP_BUF               128 //buffer to print frame
+#define ENC_INC               10 //samplerate increment/decrement
+
+#define ADC_MAX               650 
+#define ADC_MIN               203
+#define SCOPE_HEIGHT          64
 #define SCOPE_WIDTH           128
-#define ALPHA                 SCOPE_HEIGHT / ADC_MAX //conversion
+//#define ALPHA                 (float)SCOPE_HEIGHT / (float)ADC_MAX //conversion
 
-#define TRIG_VALUE      32 //useful to sync signal
+#define TRIG_VALUE      28 //useful to sync signal (middle of the screen... you got it I suppose ?)
 
-//U8G2_SSD1306_128X64_NONAME_F_HW_I2C disp(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
-U8G2_SH1106_128X64_NONAME_F_HW_I2C disp(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C disp(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+//U8G2_SH1106_128X64_NONAME_F_HW_I2C disp(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 //U8X8_SSD1306_128X64_NONAME_HW_I2C disp(/* reset=*/ U8X8_PIN_NONE);
 uint8_t x;
 
@@ -42,9 +49,11 @@ union memBuf { // a little trick to reduce memory usage
 //float t = 0.0;
 uint8_t syncState; //for trigger
 
-uint16_t SR_time = 10; // ---> samplerate
-uint8_t channelScope; //There 4 channels
+volatile uint16_t SR_time = 10; // ---> samplerate
+volatile uint8_t channelScope = 0; //There 4 channels
+uint8_t trigTimeOut;
 
+char textBuf[5];
 
 void setup() {
 
@@ -56,20 +65,20 @@ void setup() {
 
   pinMode(MUX_ADDR_A0, OUTPUT);
   pinMode(MUX_ADDR_A1, OUTPUT);
-  
+
   disp.begin();
 
 
   //Serial.println("hi");
-  
-  //animated logo display 
+
+  //animated logo display
   for (uint8_t frame = 0; frame < 12; frame++)
   {
     for (uint16_t j = 0; j < 8; j++)
     {
       for (uint16_t i = 0; i < BMP_BUF; i++)
       {
-        buf.bmp[i] = pgm_read_byte(&logo[frame*1024 + j * 128 + i]);
+        buf.bmp[i] = pgm_read_byte(&logo[frame * 1024 + j * 128 + i]);
       }
       disp.drawBitmap(0, j * 8, 16, 8, buf.bmp);
     }
@@ -85,8 +94,8 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(ENC_INT), encoderInterrupt, FALLING);
   attachInterrupt(digitalPinToInterrupt(ENC_BUT), buttonInterrupt, FALLING);
 
+  disp.setFont(TEXT_FONT_USED);
 
-  
 }
 
 
@@ -94,24 +103,24 @@ void setup() {
 
 void encoderInterrupt(void)
 {
-  if(digitalRead(ENC_DAT)) //clockwise ?
+  if (digitalRead(ENC_DAT)) //clockwise ?
   {
-    SR_time++;
-    if(SR_time > 2000)
+    SR_time += ENC_INC;
+    if (SR_time > 2000)
       SR_time = 2000;
   }
   else // or counter clockwise
   {
-    SR_time--;
-    if(SR_time < 1)
-      SR_time = 1;
+    SR_time -= ENC_INC;
+    if (SR_time < ENC_INC)
+      SR_time = ENC_INC;
   }
 }
 
 void buttonInterrupt(void)
 {
   channelScope++; //another little hack if pins used are PB0 & PB1
-  if(channelScope > 4)
+  if (channelScope >= 4)
   {
     channelScope = 0;
   }
@@ -133,25 +142,32 @@ void loop() {
 
   for (x = 0; x < SCOPE_WIDTH; x++) //conversion and write on graphic buffer
   {
-    buf.y[x] *= ALPHA;
-    disp.drawPixel(x, buf.y[x]);
+    buf.y[x] = map(buf.y[x], ADC_MIN, ADC_MAX, 0, SCOPE_HEIGHT);
   }
 
+  for (x = 1; x < SCOPE_WIDTH; x++) //conversion and write on graphic buffer
+  {
+    disp.drawLine(x-1, 63-buf.y[x-1], x, 63-buf.y[x]);
+  }
 
-
-
-  
   //update display
+  
+ 
+  
+  disp.setCursor(TEXT_CURSOR_X,TEXT_CURSOR_Y);
+  sprintf(textBuf, "CH%d", channelScope+1);
+  disp.print(textBuf);
   disp.updateDisplay();
   disp.clearBuffer();
   //display "measured area"
-  disp.drawHLine(63, 0, map(SR_time, 1, 2000, 1, 63));
-  disp.drawHLine(63-map(SR_time, 1, 2000, 1, 63), 0, map(SR_time, 1, 2000, 1, 63));
+  disp.drawHLine(63, 0, map(SR_time, ENC_INC, 2000, ENC_INC, 63));
+  disp.drawHLine(63 - map(SR_time, ENC_INC, 2000, ENC_INC, 63), 0, map(SR_time, ENC_INC, 2000, ENC_INC, 63));
   //disp.clear(); //to long to use this
   syncState = 0;
-  while (syncState < 3) //sync signal
+  trigTimeOut = 0;
+  while ((syncState < 3) && (trigTimeOut < 200)) //sync signal
   {
-    buf.y[0] = analogRead(ANALOG_IN) * ALPHA;
+    buf.y[0] = map(analogRead(A0), ADC_MIN, ADC_MAX, 0, SCOPE_HEIGHT);
 
     //Maybe think about an optimisation here ?
     if (buf.y[0] > TRIG_VALUE && syncState == 0)
@@ -166,6 +182,7 @@ void loop() {
     {
       syncState++;
     }
+    trigTimeOut++;
   }
 
 
